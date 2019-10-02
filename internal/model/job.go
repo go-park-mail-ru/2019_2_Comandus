3deaffee1f955e9438b4d89871bab7bce81a90ee
Daria Efimova <dashaefimova02@yandex.ru>
2019-10-02 17:53:42 +0300

    fix job

--- internal/model/job.go
+++ internal/model/job.go
@@ -2,14 +2,14 @@ package model
 
 type Job struct {
 	ID                int     `json:"id"`
-	HireManagerId     int     `json:"hireManagerId"`
+	HireManagerId     int     `json:"hireManagerId,string"`
 	Title             string  `json:"title"`
 	Description       string  `json:"description"`
 	Files             string  `json:"files"`
-	SpecialityId      int     `json:"specialityId"`
-	ExperienceLevelId int     `json:"experienceLevelId"`
-	PaymentAmout      float64 `json:"paymentAmount"`
+	SpecialityId      int     `json:"specialityId,string"`
+	ExperienceLevelId int     `json:"experienceLevelId,string"`
+	PaymentAmout      float64 `json:"paymentAmount,string"`
 	Country           string  `json:"country"`
 	City              string  `json:"city"`
-	JobTypeId         int     `json:"jobTypeId"`
+	JobTypeId         int     `json:"jobTypeId,string"`
 }
