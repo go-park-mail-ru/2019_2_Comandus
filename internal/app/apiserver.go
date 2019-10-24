fad3166d035244a10a068cc1a087fcd17909d260
Daria Efimova <dashaefimova02@yandex.ru>
2019-10-24 19:57:22 +0300

    create tables in function

--- internal/app/apiserver.go
+++ internal/app/apiserver.go
@@ -42,8 +42,58 @@ func newDB(dbURL string) (*sql.DB, error) {
 	// MAX CONNECTIONS
 	db.SetMaxOpenConns(10)
 
-	if err := db.Ping(); err != nil {
+	/*if err := db.Ping(); err != nil {
+		return nil, err
+	}*/
+	if err := createTables(db); err != nil {
 		return nil, err
 	}
 	return db, nil
+}
+
+func createTables(db *sql.DB) error {
+	usersQuery := `CREATE TABLE IF NOT EXISTS users (
+		accountId bigserial not null primary key,
+		firstName varchar,
+		secondName varchar,
+		userName varchar not null,
+		email varchar not null unique,
+		encryptPassword varchar not null,
+		avatar bytea,
+		userType varchar --not null
+	);`
+	if _, err := db.Exec(usersQuery); err != nil {
+		return err
+	}
+
+
+	managersQuery := `CREATE TABLE IF NOT EXISTS managers (
+		id bigserial not null primary key,
+		accountId bigserial references users,
+		registrationDate timestamp,
+		location varchar,
+		companyId bigserial --references  companies
+	);`
+	if _, err := db.Exec(managersQuery); err != nil {
+		return err
+	}
+
+	freelancersQuery := `CREATE TABLE IF NOT EXISTS freelancers (
+		id bigserial not null primary key,
+		accountId bigserial not null references users,
+		registrationDate timestamp,
+		country varchar,
+		city varchar,
+		address varchar,
+		phone varchar,
+		tagLine varchar,
+		overview varchar,
+		experienceLevelId bigserial,
+		specialityId bigserial --references specialities
+	);`
+	if _, err := db.Exec(freelancersQuery); err != nil {
+		return err
+	}
+
+	return nil
 }
