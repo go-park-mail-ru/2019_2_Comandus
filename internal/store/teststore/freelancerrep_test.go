c7554f7fec8bd78daf9a9fc8f3bc6691495db0d8
Daria Efimova <dashaefimova02@yandex.ru>
2019-10-30 11:09:27 +0300

    rewrite freelancerrep test to sqlmock

--- internal/store/teststore/freelancerrep_test.go
+++ internal/store/teststore/freelancerrep_test.go
@@ -1,93 +1,330 @@
 package teststore
 
 import (
+	"fmt"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-park-mail-ru/2019_2_Comandus/internal/model"
 	"github.com/go-park-mail-ru/2019_2_Comandus/internal/store/sqlstore"
-	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
 )
 
 func TestFreelancerRep_Create(t *testing.T) {
-	db, teardown := testStore(t, databaseURL)
-	defer teardown("users", "freelancers")
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("cant create mock: %s", err)
+	}
+	defer func() {
+		mock.ExpectClose()
+		if err := db.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
 
 	store := sqlstore.New(db)
+	rows := sqlmock.
+		NewRows([]string{"accountId"})
 
-	u := testUser(t)
-	u.Email = "freelancerrepository1@example.org"
-	if err := store.User().Create(u); err != nil {
-		t.Fatal()
+	var elemID int64 = 1
+	expect := []*model.Freelancer{
+		{ ID: elemID },
 	}
 
+	for _, item := range expect {
+		rows = rows.AddRow(item.ID)
+	}
+
+	u := testUser(t)
+	u.ID = 1
 	f := testFreelancer(t, u)
-	if err := store.Freelancer().Create(f); err != nil {
+
+	// TODO: uncomment when validation will be implemented
+	/*if err := f.Validate(); err != nil {
 		t.Fatal()
+	}*/
+
+	//ok query
+	mock.
+		ExpectQuery(`INSERT INTO freelancers`).
+		WithArgs(f.AccountId, f.RegistrationDate, f.Country, f.City, f.Address, f.Phone, f.TagLine,
+			f.Overview, f.ExperienceLevelId, f.SpecialityId).
+		WillReturnRows(rows)
+
+	err = store.Freelancer().Create(f)
+	if err != nil {
+		t.Errorf("unexpected err: %s", err)
+		return
+	}
+
+	if f.ID != 1 {
+		t.Errorf("bad id: want %v, have %v", u.ID, 1)
+		return
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+
+	// query error
+	mock.
+		ExpectQuery(`INSERT INTO freelancers`).
+		WithArgs(f.AccountId, f.RegistrationDate, f.Country, f.City, f.Address, f.Phone, f.TagLine,
+			f.Overview, f.ExperienceLevelId, f.SpecialityId).
+		WillReturnError(fmt.Errorf("bad query"))
+
+	err = store.Freelancer().Create(f)
+	if err == nil {
+		t.Errorf("expected error, got nil")
+		return
+	}
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
 
-// TODO: add not found case
 func TestFreelancerRep_Find(t *testing.T) {
-	db, teardown := testStore(t, databaseURL)
-	defer teardown("freelancers")
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("cant create mock: %s", err)
+	}
+	defer func() {
+		mock.ExpectClose()
+		if err := db.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	var elemID int64 = 1
+
+	// good query
+	rows := sqlmock.
+		NewRows([]string{"id", "accountId", "registrationDate", "country", "city", "address", "phone", "tagLine",
+		"overview", "experienceLevelId", "specialityId" })
+
+	u := testUser(t)
+	u.ID = elemID + 1
+	expect := []*model.Freelancer{
+		testFreelancer(t, u),
+	}
+
+	for _, item := range expect {
+		rows = rows.AddRow(item.ID, item.AccountId, item.RegistrationDate, item.Country, item.City, item.Address,
+			item.Phone, item.TagLine, item.Overview, item.ExperienceLevelId, item.SpecialityId)
+	}
+
+	mock.
+		ExpectQuery("SELECT id, accountId, registrationDate, country, city, address, phone, tagLine, " +
+		"overview, experienceLevelId, specialityId FROM freelancers WHERE").
+		WithArgs(elemID).
+		WillReturnRows(rows)
 
 	store := sqlstore.New(db)
 
-	u := testUser(t)
-	u.Email = "freelancerrepository2@example.org"
-	if err := store.User().Create(u); err != nil {
-		t.Fatal()
+	item, err := store.Freelancer().Find(elemID)
+	if err != nil {
+		t.Errorf("unexpected err: %s", err)
+		return
+	}
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+		return
 	}
 
-	f := testFreelancer(t, u)
-	if err := store.Freelancer().Create(f); err != nil {
-		t.Fatal()
+	if !reflect.DeepEqual(item, expect[0]) {
+		t.Errorf("results not match, want %v, have %v", expect[0], item)
+		return
 	}
 
-	f1, err := store.Freelancer().Find(f.ID)
-	assert.NotNil(t, f1)
-	assert.NoError(t, err)
+	// query error
+	mock.
+		ExpectQuery("SELECT id, accountId, registrationDate, country, city, address, phone, tagLine, " +
+		"overview, experienceLevelId, specialityId FROM freelancers WHERE").
+		WithArgs(elemID).
+		WillReturnError(fmt.Errorf("db_error"))
+
+	_, err = store.Freelancer().Find(elemID)
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+		return
+	}
+
+	if err == nil {
+		t.Errorf("expected error, got nil")
+		return
+	}
+
+	// row scan error
+	expect = []*model.Freelancer{
+		testFreelancer(t, u),
+	}
+
+	mock.
+		ExpectQuery("SELECT id, accountId, registrationDate, country, city, address, phone, tagLine, " +
+		"overview, experienceLevelId, specialityId FROM freelancers WHERE").
+		WithArgs(elemID).
+		WillReturnRows(rows)
+
+	_, err = store.Freelancer().Find(elemID)
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+		return
+	}
+	if err == nil {
+		t.Errorf("expected error, got nil")
+		return
+	}
 }
 
 func TestFreelancerRep_FindByUser(t *testing.T) {
-	db, teardown := testStore(t, databaseURL)
-	defer teardown("users", "freelancers")
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("cant create mock: %s", err)
+	}
+	defer func() {
+		mock.ExpectClose()
+		if err := db.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	var elemID int64 = 1
+
+	// good query
+	rows := sqlmock.
+		NewRows([]string{"id", "accountId", "registrationDate", "country", "city", "address", "phone", "tagLine",
+			"overview", "experienceLevelId", "specialityId" })
+
+	u := testUser(t)
+	u.ID = elemID + 1
+	expect := []*model.Freelancer{
+		testFreelancer(t, u),
+	}
+
+	for _, item := range expect {
+		rows = rows.AddRow(item.ID, item.AccountId, item.RegistrationDate, item.Country, item.City, item.Address,
+			item.Phone, item.TagLine, item.Overview, item.ExperienceLevelId, item.SpecialityId)
+	}
+
+	mock.
+		ExpectQuery("SELECT id, accountId, registrationDate, country, city, address, phone, tagLine, " +
+			"overview, experienceLevelId, specialityId FROM freelancers WHERE").
+		WithArgs(u.ID).
+		WillReturnRows(rows)
 
 	store := sqlstore.New(db)
 
-	u := testUser(t)
-	u.Email = "freelancerrepository3@example.org"
-	if err := store.User().Create(u); err != nil {
-		t.Fatal()
+	item, err := store.Freelancer().FindByUser(u.ID)
+	if err != nil {
+		t.Errorf("unexpected err: %s", err)
+		return
+	}
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+		return
 	}
 
-	f := testFreelancer(t, u)
-	if err := store.Freelancer().Create(f); err != nil {
-		t.Fatal()
+	if !reflect.DeepEqual(item, expect[0]) {
+		t.Errorf("results not match, want %v, have %v", expect[0], item)
+		return
 	}
 
-	f1, err := store.Freelancer().FindByUser(u.ID)
-	assert.NotNil(t, f1)
-	assert.NoError(t, err)
+	// query error
+	mock.
+		ExpectQuery("SELECT id, accountId, registrationDate, country, city, address, phone, tagLine, " +
+			"overview, experienceLevelId, specialityId FROM freelancers WHERE").
+		WithArgs(u.ID).
+		WillReturnError(fmt.Errorf("db_error"))
+
+	_, err = store.Freelancer().FindByUser(u.ID)
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+		return
+	}
+
+	if err == nil {
+		t.Errorf("expected error, got nil")
+		return
+	}
+
+	// row scan error
+	expect = []*model.Freelancer{
+		testFreelancer(t, u),
+	}
+
+	mock.
+		ExpectQuery("SELECT id, accountId, registrationDate, country, city, address, phone, tagLine, " +
+			"overview, experienceLevelId, specialityId FROM freelancers WHERE").
+		WithArgs(u.ID).
+		WillReturnRows(rows)
+
+	_, err = store.Freelancer().FindByUser(u.ID)
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+		return
+	}
+	if err == nil {
+		t.Errorf("expected error, got nil")
+		return
+	}
 }
 
 func TestFreelancerRep_Edit(t *testing.T) {
-	db, teardown := testStore(t, databaseURL)
-	defer teardown("users", "freelancers")
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("cant create mock: %s", err)
+	}
+
+	defer func() {
+		mock.ExpectClose()
+		if err := db.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
 
 	store := sqlstore.New(db)
 
+	rows := sqlmock.
+		NewRows([]string{"accountId"})
+
+	var elemID int64 = 1
+	expect := []*model.User{
+		{ ID: elemID },
+	}
+
+	for _, item := range expect {
+		rows = rows.AddRow(item.ID)
+	}
+
 	u := testUser(t)
-	u.Email = "freelancerrepository4@example.org"
-	if err := store.User().Create(u); err != nil {
-		t.Fatal()
-	}
-
+	u.ID = 1
 	f := testFreelancer(t, u)
-	if err := store.Freelancer().Create(f); err != nil {
-		t.Fatal()
-	}
-	f.City = "London"
+	f.ID = 1
 
-	if err := store.Freelancer().Edit(f); err != nil {
+	// TODO: uncomment when validation will be implemented
+	/*if err := f.Validate(); err != nil {
 		t.Fatal()
+	}*/
+
+	//ok query
+	f.Country = "England"
+	f.City = "London"
+	mock.
+		ExpectQuery(`UPDATE freelancers SET`).
+		WithArgs(f.Country, f.City, f.Address, f.Phone, f.TagLine,
+		f.Overview, f.ExperienceLevelId, f.SpecialityId, f.ID).
+		WillReturnRows(rows)
+
+	err = store.Freelancer().Edit(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if u.ID != 1 {
+		t.Errorf("bad id: want %v, have %v", u.ID, 1)
+		return
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
