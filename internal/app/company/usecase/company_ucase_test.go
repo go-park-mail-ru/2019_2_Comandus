632bc4f6e06c97b2399ef74a3fd12d1ec028a337
Andronovdima <hifromnorway@gmail.com>
2019-11-27 10:50:08 +0300

    add to company usecase

--- internal/app/company/usecase/company_ucase_test.go
+++ internal/app/company/usecase/company_ucase_test.go
@@ -1,28 +1,28 @@
 package companyUsecase
 
 import (
-	"errors"
 	"github.com/go-park-mail-ru/2019_2_Comandus/internal/app/company"
+	"github.com/go-park-mail-ru/2019_2_Comandus/internal/app/manager/delivery/grpc/manager_grpc"
 	"github.com/go-park-mail-ru/2019_2_Comandus/internal/app/mocks/client_mocks"
 	"github.com/go-park-mail-ru/2019_2_Comandus/internal/app/mocks/repository_mocks"
 	"github.com/go-park-mail-ru/2019_2_Comandus/internal/model"
 	"github.com/golang/mock/gomock"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func testUcase(t *testing.T) (*repository_mocks.MockCompanyRepository, company.Usecase){
+func testUcase(t *testing.T) (*repository_mocks.MockCompanyRepository, *client_mocks.MockManagerClient ,company.Usecase){
 	t.Helper()
 	ctrl := gomock.NewController(t)
 	companyRep := repository_mocks.NewMockCompanyRepository(ctrl)
-	companyClient := client_mocks.NewMockCompanyClient(ctrl)
 	managerClient := client_mocks.NewMockManagerClient(ctrl)
-	companyUcase := NewCompanyUsecase(companyRep, companyClient, managerClient)
-	return companyRep, companyUcase
+	companyUcase := NewCompanyUsecase(companyRep, managerClient)
+	return companyRep, managerClient,  companyUcase
 }
 
 func TestCompanyUsecase_Create(t *testing.T) {
-	companyRep, companyUcase := testUcase(t)
+	companyRep, _ ,companyUcase := testUcase(t)
 
 	testCases := []struct {
 		name			string
@@ -67,7 +67,7 @@ func TestCompanyUsecase_Create(t *testing.T) {
 }
 
 func TestCompanyUsecase_Edit(t *testing.T) {
-	companyRep, companyUcase := testUcase(t)
+	companyRep, managerClient, companyUcase := testUcase(t)
 
 	user := &model.User{
 		ID:               1,
@@ -80,12 +80,18 @@ func TestCompanyUsecase_Edit(t *testing.T) {
 		newCompany		*model.Company
 		userType		string
 		expectError		error
+		expectedManager *manager_grpc.Manager
 	}{
 		{
 			name:			"valid",
 			newCompany:		&model.Company{
+				ID: 		1,
 				CompanyName: "new name",
 				Site:        "www.new-site.ru",
+			},
+			expectedManager: &manager_grpc.Manager{
+				ID:                   1,
+				CompanyId:            1,
 			},
 			userType:		model.UserCustomer,
 			expectError:	nil,
@@ -93,22 +99,32 @@ func TestCompanyUsecase_Edit(t *testing.T) {
 		{
 			name:			"invalid: user is freelancer",
 			newCompany:		&model.Company{
+				ID: 		 1,
 				CompanyName: "new name",
 				Site:        "www.new-site.ru",
 			},
+			expectedManager: &manager_grpc.Manager{
+				ID:                   1,
+				CompanyId:            1,
+			},
 			userType:		model.UserFreelancer,
-			expectError:	errors.New("only manager can edit company"),
+			expectError:	errors.New(" only manager can edit company"),
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			user.UserType = tc.userType
+			managerClient.
+				EXPECT().
+				GetManagerByUserFromServer(user.ID).
+				Return(tc.expectedManager, nil)
+
 
 			companyRep.
 				EXPECT().
 				Edit(tc.newCompany).
-				Return(nil)
+				Return(tc.expectError)
 
 			err := companyUcase.Edit(user, tc.newCompany)
 
@@ -117,46 +133,48 @@ func TestCompanyUsecase_Edit(t *testing.T) {
 				return
 			}
 
+			expectError := "HandleEditCompany<-Edit: :  only manager can edit company"
+
 			if err != nil {
-				assert.Equal(t, tc.expectError.Error(), err.Error())
+				assert.Equal(t, expectError, err.Error())
 				return
 			}
-
 			t.Fatal()
 		})
 	}
 }
 
-//func TestCompanyUsecase_Find(t *testing.T) {
-//	companyRep, _, companyUcase := testUcase(t)
-//
-//	testCases := []struct {
-//		name        string
-//		company     *model.Company
-//		expectError error
-//	}{
-//		{
-//			name:			"valid",
-//			company:		&model.Company{
-//				ID:          1,
-//				CompanyName: "test company",
-//				Site:        "test",
-//			},
-//			expectError:	nil,
-//		},
-//	}
-//
-//	for _, tc := range testCases {
-//		t.Run(tc.name, func(t *testing.T) {
-//			var err error
-//			companyRep.
-//				EXPECT().
-//				Find(tc.company.ID).
-//				Return(tc.company, err)
-//
-//			_, err = companyUcase.Find(tc.company.ID)
-//
-//			assert.Equal(t, nil, err)
-//		})
-//	}
-//}
+func TestCompanyUsecase_Find(t *testing.T) {
+	companyRep, _, companyUcase := testUcase(t)
+
+	testCases := []struct {
+		name        string
+		company     *model.Company
+		expectError error
+	}{
+		{
+			name:			"valid",
+			company:		&model.Company{
+				ID:          1,
+				CompanyName: "test company",
+				Site:        "test",
+			},
+			expectError:	nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var err error
+			companyRep.
+				EXPECT().
+				Find(tc.company.ID).
+				Return(tc.company, err)
+
+			company, err := companyUcase.Find(tc.company.ID)
+
+			assert.Equal(t, nil, err)
+			assert.Equal(t, company, testCases[0].company)
+		})
+	}
+}
