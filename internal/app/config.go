0e95ad32083db2a81bf5d0861fba3a92ed8e3cf9
Andronovdima <hifromnorway@gmail.com>
2019-12-15 17:10:16 +0300

    config.go

--- internal/app/config.go
+++ internal/app/config.go
@@ -14,8 +14,8 @@ func NewConfig() *Config {
 		BindAddr:   ":8080",
 		LogLevel:   "debug",
 		SessionKey: "jdfhdfdj",
-		DatabaseURL: "host=localhost dbname=restapi_dev sslmode=disable port=5432 password=1234 user=d",
-		//DatabaseURL: "host=localhost dbname=restapi_dev sslmode=disable port=5432 password=1234 user=ubuntu",
+		//DatabaseURL: "host=localhost dbname=restapi_dev sslmode=disable port=5432 password=1234 user=d",
+		DatabaseURL: "host=localhost dbname=restapi_dev sslmode=disable port=5432 password=1234 user=ubuntu",
 		TokenSecret: "golangsecpark",
 		//ClientUrl:		"http://127.0.0.1:9000", //"http://localhost:9000",//
 		ClientUrl: "http://89.208.211.100:9000",
